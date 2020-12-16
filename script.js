94f9a718d05e87d8ed39a062506c42e35c8cf1d0
Hites Dániel <hitesdani@gmail.com>
2020-12-16 09:34:18 +0100

    basic structure created

---
+++ script.js
@@ -0,0 +1,14 @@
+function pageLoad() {
+    let arr = [
+        {
+            "tag": "h1",
+            "content": "Dani"
+
+        }
+    ];
+    console.log(arr[0]);
+
+
+}
+
+window.addEventListener('load', pageLoad);
