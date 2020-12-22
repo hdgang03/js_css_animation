9eb71e7c5cae37fb0b0d8bbf0fa0946b8746b227
Hites Dániel <hitesdani@gmail.com>
2020-12-22 10:56:12 +0100

    done

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 function pageLoad() {
-    let arr = [
-        {
-            "tag": "h1",
-            "content": "Dani"
+    let objectPlace = document.getElementById('root');
 
+    let myObjects = [
+        {
+            tag: "h1",
+            content: "Hites Dániel"
+
+        },
+        {
+            tag: "p",
+            content: "Frontend Developer"
         }
     ];
-    console.log(arr[0]);
+    /*  
+      for (let i = 0; i < myObjects.length; i++) {
+          
+      }
+    */
 
+    for (myObject of myObjects) {
+        let newElem = document.createElement(myObject.tag);
+        newElem.innerText = myObject.content;
+        objectPlace.appendChild(newElem);
+    }
+
+    objectPlace.addEventListener('click', function () {
+        objectPlace.classList.toggle("clicked");
+    });
 
 }
 
